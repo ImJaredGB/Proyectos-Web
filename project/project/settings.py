3ad553d3d8b9fbc213f3ff5ea12ae0ae5e0df449
ImJaredGB <jaredalejandro.gb@gmail.com>
2025-08-05 20:25:41 -0500

    carga de los statics de forma correcta (ubicacion correcta de los styles)

--- project/project/settings.py
+++ project/project/settings.py
@@ -115,11 +115,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
